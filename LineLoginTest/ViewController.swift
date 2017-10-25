007907ea462f5cceae37c2559480e104c50e8855
h8254201 <h82542011@gmail.com>
2017-10-25 14:50:23 +0800

    init

--- LineLoginTest/ViewController.swift
+++ LineLoginTest/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,LineSDKLoginDelegate {
 
+    @IBAction func at(_ sender: Any) {
+        LineSDKLogin.sharedInstance().start()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        LineSDKLogin.sharedInstance().delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func didLogin(_ login: LineSDKLogin, credential: LineSDKCredential?, profile: LineSDKProfile?, error: Error?) {
+        if let e = error{
+            print(e)
+        }else{
+            print(profile?.userID ?? "")
+            print(profile?.displayName ?? "")
+            print(profile?.statusMessage ?? "")
+        }
+    }
 
 }
 
